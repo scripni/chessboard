babd306b2b071847ea8a9c935c41e43ed94e0d04
Andrei Scripniciuc <andreisc@outlook.com>
2013-02-17 20:41:07 +0000

    Added ability to move pieces

--- js/chessboard.js
+++ js/chessboard.js
@@ -5,9 +5,33 @@
 var pieceSizeRatio = 0.8;
 
 $(document).ready(function () {
+    setMoveOnClick();
     $(window).resize(resizePieces);
     resizePieces();
 });
+
+function setMoveOnClick() {
+    /// <summary>Attaches the click handler to all chess pieces on the board.</summary>
+    var squares = $("#chessboard div div");
+    squares.click(function () {
+        if ($("a", this).length > 0) {
+            $(this).addClass("selected");
+        } else {
+            var selectedPiece = $("div.selected a").first();
+            if (selectedPiece) {
+                $(this).append($("div.selected>a"));
+            }
+        };
+    });
+    $("a", chessboard).click(function () {
+        if ($(this).hasClass("selected")) {
+            $(this).parent().removeClass("selected");
+        } else {
+            $("div div", chessboard).removeClass("selected");
+            $(this).parent().addClass("selected");
+        }
+    });
+}
 
 function resizePieces() {
     /// <summary>Recalculates the piece size based on the size of their containing squares.</summary>

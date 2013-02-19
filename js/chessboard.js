bc53093a6df2033c8330d3e77493305754adf812
Andrei Scripniciuc <andreisc@outlook.com>
2013-02-19 21:34:18 +0000

    fixed jslint errors

--- js/chessboard.js
+++ js/chessboard.js
@@ -1,6 +1,7 @@
-(function (document, window) {
-    var chessboards = {};
+/*jslint browser:true, bitwise:true */
 
+(function (document, window) {
+    "use strict";
     function createChessPiece(type) {
         /// <summary>Creates an anchor containing a chess piece of the specified type.</summary>
         var piece = document.createElement("a");
@@ -47,11 +48,13 @@
             default:
                 piece.innerHTML = "X";
                 break;
-        };
+        }
         return piece;
-    };
+    }
 
-    var chessboard = window.chessboard = function (boardId) {
+    var chessboards = {};
+
+    window.chessboard = function (boardId) {
 
         // fallback to the default id if no id was specified
         boardId = boardId || "chessboard";
@@ -62,104 +65,107 @@
         }
 
         // only initialize each board once
-        var initialized = false;
+        var initialized = false,
+            selectedSquare = null,
+            // get the root DOM element
+            root = document.getElementById(boardId),
+            init = function () {
+                if (initialized) { return; }
 
-        var selectedSquare = null;
-
-        // get the root DOM element
-        var root = document.getElementById(boardId);
-
-        var init = function () {
-            if (initialized) { return; }
-
-            function resizePieces() {
-                var anchors = root.getElementsByTagName('a');
-                var squareSize = root.getElementsByTagName('div')[0].getElementsByTagName('div')[0].clientWidth;
-                var pieceRatio = 0.7;
-                var pieceSize = squareSize * pieceRatio;
-                var margin = (squareSize - pieceSize) / 2;
-                for (var i = 0; i < anchors.length; i++) {
-                    var anchor = anchors[i];
-                    anchor.style.fontSize = pieceSize + "px";
-                    anchor.style.margin = margin + "px";
-                }
-            };
-
-            window.onresize = resizePieces;
-
-            var squares = new Array();
-
-            // add rows
-            for (var r = 0; r < 8; r++) {
-                var row = document.createElement("div");
-
-                // add cells
-                for (var c = 0; c < 8; c++) {
-                    var cell = document.createElement("div");
-
-                    // set the onclick event to handle piece movement
-                    cell.onclick = function () {
-
-                        if (selectedSquare == this) {
+                var squares = [],
+                    r = 0,
+                    c = 0,
+                    i = 0,
+                    row = null,
+                    cell = null,
+                    movePiece = function () {
+                        if (selectedSquare === this) {
                             selectedSquare.classList.remove('selected');
                             selectedSquare = null;
-                        } else if (this.getElementsByTagName('a').length == 0 && selectedSquare != null) {
+                        } else if (this.getElementsByTagName('a').length === 0 && selectedSquare !== null) {
                             this.appendChild(selectedSquare.getElementsByTagName('a')[0]);
                             selectedSquare.classList.remove('selected');
                             selectedSquare = null;
-                        } else if (this.getElementsByTagName('a').length > 0 && selectedSquare == null) {
+                        } else if (this.getElementsByTagName('a').length > 0 && selectedSquare === null) {
                             selectedSquare = this;
                             selectedSquare.classList.add('selected');
                         }
                     };
 
-                    row.appendChild(cell);
-                    squares.push(cell);
-                };
+                function resizePieces() {
+                    var anchors = root.getElementsByTagName('a'),
+                        squareSize = root.getElementsByTagName('div')[0].getElementsByTagName('div')[0].clientWidth,
+                        pieceRatio = 0.7,
+                        pieceSize = squareSize * pieceRatio,
+                        margin = (squareSize - pieceSize) / 2,
+                        i = 0,
+                        anchor = null;
 
-                root.appendChild(row);
+                    for (i = 0; i < anchors.length; i++) {
+                        anchor = anchors[i];
+                        anchor.style.fontSize = pieceSize + "px";
+                        anchor.style.margin = margin + "px";
+                    }
+                }
+
+                window.onresize = resizePieces;
+
+                // add rows
+                for (r = 0; r < 8; r++) {
+                    row = document.createElement("div");
+
+                    // add cells
+                    for (c = 0; c < 8; c++) {
+                        cell = document.createElement("div");
+
+                        // set the onclick event to handle piece movement
+                        cell.onclick = movePiece;
+
+                        row.appendChild(cell);
+                        squares.push(cell);
+                    }
+
+                    root.appendChild(row);
+                }
+
+                // add the chess pieces for the starting position
+
+                squares[0].appendChild(createChessPiece("b-rook"));
+                squares[1].appendChild(createChessPiece("b-knight"));
+                squares[2].appendChild(createChessPiece("b-bishop"));
+                squares[3].appendChild(createChessPiece("b-queen"));
+                squares[4].appendChild(createChessPiece("b-king"));
+                squares[5].appendChild(createChessPiece("b-bishop"));
+                squares[6].appendChild(createChessPiece("b-knight"));
+                squares[7].appendChild(createChessPiece("b-rook"));
+
+                for (i = 0; i < 8; i++) {
+                    squares[8 + i].appendChild(createChessPiece("b-pawn"));
+                    squares[8 * 6 + i].appendChild(createChessPiece("w-pawn"));
+                }
+
+                squares[8 * 7].appendChild(createChessPiece("w-rook"));
+                squares[8 * 7 + 1].appendChild(createChessPiece("w-knight"));
+                squares[8 * 7 + 2].appendChild(createChessPiece("w-bishop"));
+                squares[8 * 7 + 3].appendChild(createChessPiece("w-queen"));
+                squares[8 * 7 + 4].appendChild(createChessPiece("w-king"));
+                squares[8 * 7 + 5].appendChild(createChessPiece("w-bishop"));
+                squares[8 * 7 + 6].appendChild(createChessPiece("w-knight"));
+                squares[8 * 7 + 7].appendChild(createChessPiece("w-rook"));
+
+                resizePieces();
+                initialized = true;
             };
 
-            // add the chess pieces for the starting position
-
-            squares[0].appendChild(createChessPiece("b-rook"));
-            squares[1].appendChild(createChessPiece("b-knight"));
-            squares[2].appendChild(createChessPiece("b-bishop"));
-            squares[3].appendChild(createChessPiece("b-queen"));
-            squares[4].appendChild(createChessPiece("b-king"));
-            squares[5].appendChild(createChessPiece("b-bishop"));
-            squares[6].appendChild(createChessPiece("b-knight"));
-            squares[7].appendChild(createChessPiece("b-rook"));
-
-            for (var i = 0; i < 8; i++) {
-                squares[8 + i].appendChild(createChessPiece("b-pawn"));
-                squares[8 * 6 + i].appendChild(createChessPiece("w-pawn"));
-            }
-
-            squares[8 * 7].appendChild(createChessPiece("w-rook"));
-            squares[8 * 7 + 1].appendChild(createChessPiece("w-knight"));
-            squares[8 * 7 + 2].appendChild(createChessPiece("w-bishop"));
-            squares[8 * 7 + 3].appendChild(createChessPiece("w-queen"));
-            squares[8 * 7 + 4].appendChild(createChessPiece("w-king"));
-            squares[8 * 7 + 5].appendChild(createChessPiece("w-bishop"));
-            squares[8 * 7 + 6].appendChild(createChessPiece("w-knight"));
-            squares[8 * 7 + 7].appendChild(createChessPiece("w-rook"));
-
-            resizePieces();
-            initialized = true;
+        chessboards["chessboard-root-" + boardId] = {
+            init: init
         };
-
-        return (chessboards["chessboard-root-" + boardId] = {
-            init: init,
-        });
-    }
-})(document, window);
-
-function ChessEngine() {
-
-};
+        return chessboards["chessboard-root-" + boardId];
+    };
+}(document, window));
 
 function ChessPiece(type, row, column) {
+    "use strict";
     // all data will be stored on 64 bits (32 + 32)
     // each bit represents one board square
     // to keep it simple, black will always be at the top of the board
@@ -171,33 +177,41 @@ function ChessPiece(type, row, column) {
     this.move_squares = [0x00000000, 0x00000000];
     this.pieceType = type;
     this.setPosition(row, column);
-};
+}
 
 ChessPiece.prototype.getColumn = function () {
+    "use strict";
     // the column is the same every 8th cell
     // cell indexing starts from 0
     if (this.position[0] > 0) {
         return 7 - Math.floor(Math.log(this.position[0]) / Math.LN2 % 8);
-    } else {
-        return 7 - Math.floor(Math.log(this.position[1]) / Math.LN2 % 8);
     }
+    return 7 - Math.floor(Math.log(this.position[1]) / Math.LN2 % 8);
 };
 
 ChessPiece.prototype.getRow = function () {
+    "use strict";
+    var r = 0,
+        c = 0,
+        val = null;
     // the row increases every 8 cells
     if (this.position[1] > 0) {
-        var val = this.position[1];
-        for (var r = 0; r < 4; r++) {
-            for (var c = 0; c < 8; c++) {
-                if (val == 1) return r;
+        val = this.position[1];
+        for (r = 0; r < 4; r++) {
+            for (c = 0; c < 8; c++) {
+                if (val === 1) {
+                    return r;
+                }
                 val = val >>> 1;
             }
         }
     } else {
-        var val = this.position[0];
-        for (var r = 4; r < 8; r++) {
-            for (var c = 0; c < 8; c++) {
-                if (val == 1) return r;
+        val = this.position[0];
+        for (r = 4; r < 8; r++) {
+            for (c = 0; c < 8; c++) {
+                if (val === 1) {
+                    return r;
+                }
                 val = val >>> 1;
             }
         }
@@ -205,6 +219,7 @@ ChessPiece.prototype.getRow = function () {
 };
 
 ChessPiece.prototype.setPosition = function (row, column) {
+    "use strict";
     // the position is stored as 2 ^ ((8 * row) + column)
     var pow = 8 * row + (7 - column);
     if (pow > 31) {
